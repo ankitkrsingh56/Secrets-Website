cb2986426e0784f981c1b3223f47900e2a677b36
ankitkrsingh56 <ankitsinghix05@gmail.com>
2020-05-28 18:22:26 +0530

    Level 2 Authentication using mongoose-encryption

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -18,7 +21,8 @@ const userSchema = new mongoose.Schema({
  email: String,
  password: String,
 });
-
+const secret = "Thisisourlittlesecret."
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]}  );
 const User = new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
